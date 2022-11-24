c00ba9068f9dc414eac07646e60d8f870381aa35
chenxianyang986 <xc29@illinois.edu>
2022-11-23 23:17:15 -0600

    bug meam not working

--- meam_calculator.py
+++ meam_calculator.py
@@ -2,17 +2,13 @@ from ase.calculators.lammpsrun import LAMMPS
 import os
 from ase.calculators.lammpslib import LAMMPSlib
 
+import lammps
 def setup_meam_calculator() -> LAMMPSlib:
   '''
-  os.environ["LAMMPSPATH"] = "C:\\Users\\xianyang chen\\mylammps"
-  imp_path = os.environ['LAMMPSPATH']
-  public_file_name = os.path.join(imp_path, "library.meam")  # type: ignore
-  '''
-  '''
   parameters = {'pair_style': 'meam', 'pair_coeff': ['* * library.meam.fs Zr']}
-  #local_file_name = os.path.join(imp_path, "Zr.meam")  # type: ignore
   files = {"Zr.meam.fs"}
   return LAMMPS(parameters = parameters, files=files)
   '''
-  cmd = {'pair_style': "lj/cut 2.5", 'pair_coeff':["* * 1 1"]}
-  return LAMMPSlib(lmpcmd = cmd)
+  cmd = ["pair_style meam",
+        "pair_coeff * * library.meam Zr NULL Zr"]
+  return LAMMPSlib(lmpcmds = cmd)
--- test_meam.py
+++ test_meam.py
@@ -3,25 +3,37 @@ import numpy as np
 from ase import Atoms, Atom
 import matplotlib.pyplot as plt
 import sys
+from ase.build import bulk
+from ase.visualize import view
 
 def test_meam_potential():
   calc = meamc.setup_meam_calculator()
-  lengths = np.linspace(1.8, 5, 301)
+  lengths = np.linspace(1, 5, 301)
   potentials = []
   atomic_distances = []
+
+  
   for i in range(len(lengths)):
-      atoms = Atoms(2 * "Zr", positions=[[0, 0, 0], [0, 0, lengths[i]]])
-      atoms.calc = calc
-      
-      potentials.append(atoms.get_potential_energy())
-      atomic_distances.append(lengths[i])
-      print(lengths[i], potentials[i])
-      #view(atoms)
-      
+    atoms: Atoms = bulk('Zr', crystalstructure='hcp')
+    atoms.set_cell((atoms.get_cell().lengths() * i).tolist() + atoms.get_cell().angles().tolist(), scale_atoms=True)
+    print(atoms.get_cell())
+    atoms.calc = calc
+    potentials.append(atoms.get_potential_energy())
+    atomic_distances.append(lengths[i])
   plt.plot(atomic_distances, potentials, color='green', linestyle='dashed')
   plt.xlabel("r Angstrom")
   plt.ylabel("Energy ev")
   plt.show()
+  
+  #Ni = bulk('Ni', cubic=True)
+  #Fe = Atom('Fe', position=Ni.cell.diagonal()/2)
+  #NiFe = Ni + Fe
+  #NiFe.calc = calc
+  #print("Energy ", NiFe.get_potential_energy())
+  #Zr_1 = Atom('Fe', position=Zr.cell.diagonal()/2)
+  #Zr2 = Zr + Zr_1
+  #Zr2.calc = calc
+  #print("Energy ", Zr2.get_potential_energy())
 
 def main(argv):
   if argv[0] == "--test_meam":
